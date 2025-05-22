f506cd3c15933f2940412a1ac1a7df26d3e45ce1
azevedop <gdz2017enterprise@gmail.com>
2025-05-22 19:50:02 +0100

    Merge branch 'master' of https://github.com/pedro-azevedo-ua/as-final-s3

---
+++ src/ContentsRUs.Eventing.Models/EventDetails.cs
@@ -0,0 +1,9 @@
+namespace ContentsRUs.Eventing.Models
+{
+    public class EventDetails
+    {
+        public string Title { get; set; }
+        public string Description{ get; set; }
+        public object Body { get; set; }
+    }
+}
